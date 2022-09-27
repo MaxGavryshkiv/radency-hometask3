6ffd8a638053751c18b61c15d6417f432f2a6acf
Max Gavryshkiv <maks20355@gmail.com>
2022-09-27 16:00:24 +0300

    created routes

---
+++ routes/notes/index.ts
@@ -0,0 +1,28 @@
+(function () {
+  const express = require("express");
+  const router = express.Router();
+
+  const ctrl = require("../../controllers/index.ts");
+
+  const {
+    validationCreateNote,
+    validationUpdateNote,
+  } = require("../../services/validation");
+
+  router.get("/stats", ctrl.getNotesSummary);
+  router.get("/", ctrl.getAllNotes);
+  router.get("/archive", ctrl.getAllArchiveNotes);
+  router.get("/:id", ctrl.getNoteById);
+
+  router.post("/", validationCreateNote, ctrl.addNote);
+
+  router.patch("/:id", validationUpdateNote, ctrl.updateNote);
+
+  router.patch("/archive/:id", ctrl.archiveNote);
+  router.patch("/unarchive/:id", ctrl.unarchiveNote);
+
+  router.delete("/:id", ctrl.removeNote);
+  router.delete("/archive/:id", ctrl.removeArchiveNote);
+
+  module.exports = router;
+})();
